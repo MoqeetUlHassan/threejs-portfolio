85cf15275fe80e552cad90f8dad8250d40949274
moqeet <moqeet1998@gmail.com>
2025-06-02 00:58:15 +0500

    paths

--- src/components/DemoComputer.jsx
+++ src/components/DemoComputer.jsx
@@ -9,7 +9,7 @@ import { useGSAP } from '@gsap/react';
 
 const DemoComputer = (props) => {
   const group = useRef();
-  const { nodes, materials, animations } = useGLTF('./models/computer.glb');
+  const { nodes, materials, animations } = useGLTF('./threejs-portfolio/models/computer.glb');
   const { actions } = useAnimations(animations, group);
 
   const txt = useVideoTexture(props.texture ? props.texture : '/textures/project/project1.mp4');
@@ -1011,6 +1011,6 @@ const DemoComputer = (props) => {
   );
 };
 
-useGLTF.preload('./models/computer.glb');
+useGLTF.preload('./threejs-portfolio/models/computer.glb');
 
 export default DemoComputer;
