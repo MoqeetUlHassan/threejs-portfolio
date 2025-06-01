af2ce9fbafa88c4d96ee37a8b3c2fd8798b8dc63
Moqeet Ul Hassan <52046488+MoqeetUlHassan@users.noreply.github.com>
2025-06-01 23:31:47 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/threejs-portfolio/', // <-- Your repo name
   plugins: [react()],
-})
+});
